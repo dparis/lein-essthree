d0958ee8901ad32cabe73f534867c255096feb87
Dylan Paris <dylan.paris+github@gmail.com>
2015-09-24 13:34:45 -0700

    Define schemas and add project dependencies

--- project.clj
+++ project.clj
@@ -3,4 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :min-lein-version "2.0.0"
+
+  :dependencies [[amazonica "0.3.33"]
+                 [prismatic/schema "1.0.1"]]
+
   :eval-in-leiningen true)
---
+++ src/leiningen/essthree/schemas.clj
@@ -0,0 +1,46 @@
+(ns leiningen.essthree.schemas
+  (:require [schema.core :as s]))
+
+
+(s/defschema AWSCreds
+  {:access-key-id     s/Str
+   :secret-access-key s/Str})
+
+(s/defschema ^:private BaseS3Config
+  {:bucket                     s/Str
+   :path                       s/Str
+
+   (s/optional-key :aws-creds) AWSCreds})
+
+(s/defschema ^:private BaseDeployConfig
+  (merge BaseS3Config
+         {:type (s/enum :uberjar :library :directory)}))
+
+(s/defschema UberjarDeployConfig
+  BaseDeployConfig)
+
+(s/defschema LibraryDeployConfig
+  (merge BaseDeployConfig
+         {:repository-name                s/Str
+          (s/optional-key :snapshots)     s/Bool
+          (s/optional-key :sign-releases) s/Bool
+          (s/optional-key :checksum)      (s/enum :fail :warn :ignore)
+          (s/optional-key :update)        (s/enum :daily :always :never)}))
+
+(s/defschema DirectoryDeployConfig
+  (merge BaseDeployConfig
+         {:local-root s/Str}))
+
+(s/defschema ^:private DeployConfig
+  (s/conditional
+   #(= :uberjar (:type %))   UberjarDeployConfig
+   #(= :library (:type %))   LibraryDeployConfig
+   #(= :directory (:type %)) DirectoryDeployConfig
+   'valid-deploy-config?))
+
+(s/defschema RepoConfig
+  BaseS3Config)
+
+(s/defschema EssthreeConfig
+  {(s/optional-key :deploy)     DeployConfig
+   (s/optional-key :repository) RepoConfig})
