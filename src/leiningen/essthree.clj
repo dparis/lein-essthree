6ae8df5ea20c33af124dceff14ecb7af0aa7a2d7
Dylan Paris <dylan.paris+github@gmail.com>
2015-09-28 16:48:48 -0700

    Integrate all deploy methods in essthree task

--- src/leiningen/essthree.clj
+++ src/leiningen/essthree.clj
@@ -1,6 +1,12 @@
-(ns leiningen.essthree)
+(ns leiningen.essthree
+  (:require [leiningen.essthree.directory :as ed]
+            [leiningen.essthree.library :as el]
+            [leiningen.essthree.uberjar :as eu]))
 
 (defn essthree
-  "I don't do a lot."
   [project & args]
-  (println "Hi!"))
+  (when-let [deploy-type (get-in project [:essthree :deploy :type])]
+    (case deploy-type
+      :directory (ed/deploy-directory project)
+      :library   (el/deploy-library project)
+      :uberjar   (eu/deploy-uberjar project))))
