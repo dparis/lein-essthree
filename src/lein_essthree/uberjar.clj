913c94d9848799ca2110b759908507ffa12466b0
Dylan Paris <dylan.paris+github@gmail.com>
2015-09-29 19:39:15 -0700

    Remove project name from uberjar S3 object key

--- src/lein_essthree/uberjar.clj
+++ src/lein_essthree/uberjar.clj
@@ -21,14 +21,13 @@
 (s/defn ^:private put-uberjar-s3! :- (s/maybe s/Str)
   [config         :- UberjarDeployConfig
    build-category :- (s/enum "snapshots" "releases")
-   project-name   :- s/Str
    uj-path        :- s/Str]
   (let [aws-creds    (:aws-creds config)
         bucket       (:bucket config)
         path         (c/trim (:path config) "/")
         uj-artifact  (or (:artifact-name config)
                          (last (c/split uj-path "/")))
-        obj-key      (->> [path project-name build-category uj-artifact]
+        obj-key      (->> [path build-category uj-artifact]
                           (filter identity)
                           (c/join "/"))]
     (try
@@ -43,12 +42,7 @@
   "Deploy the current project as an application uberjar to S3."
   (let [config         (get-config project)
         build-category (if (pom/snapshot? project) "snapshots" "releases")
-        project-name   (:name project)
         uj-path        (compile-uberjar! project)
-        uj-obj         (put-uberjar-s3! config
-                                        build-category
-                                        project-name
-                                        uj-path)]
-
+        uj-obj         (put-uberjar-s3! config build-category uj-path)]
     (when uj-obj
       (main/info "Uploaded uberjar to" uj-obj))))
