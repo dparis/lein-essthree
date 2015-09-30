56083ef7b780b8181b99e386795f319fa4997f4f
Dylan Paris <dylan.paris+github@gmail.com>
2015-09-29 18:33:07 -0700

    Force joda-time to top of dependency load order

--- project.clj
+++ project.clj
@@ -6,12 +6,12 @@
 
   :min-lein-version "2.0.0"
 
-  :dependencies [[amazonica "0.3.33"]
+  :dependencies [[joda-time "2.8.2"]
+                 [amazonica "0.3.33"]
                  [org.springframework.build/aws-maven "5.0.0.RELEASE"
                   :exclusions [joda-time]]
                  [funcool/cuerdas "0.6.0"]
                  [me.raynes/fs "1.4.6"]
-                 [joda-time "2.8.2"]
                  [pandect "0.5.4"]
                  [pathetic "0.5.1"]
                  [prismatic/schema "1.0.1"]]
